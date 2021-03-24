926e7207a54e4d33e2e25fa6863ef4d560b625f4
Yū Kobayashi <kb10uy@kb10uy.org>
2021-03-24 21:51:16 +0900

    Move upload endpoint

---
+++ src/endpoint/media.rs
@@ -0,0 +1,45 @@
+//! Contains media endpoints.
+
+use crate::{
+    application::State,
+    session::{swap_flashes, Common, Flash},
+    template,
+};
+
+use async_std::sync::Arc;
+use std::io::{prelude::*, Cursor};
+
+use multipart::server::Multipart;
+use tide::{
+    http::{mime, StatusCode},
+    Redirect, Request, Response, Result as TideResult,
+};
+use yarte::Template;
+
+/// POST `/upload`
+/// Uploads a file.
+pub async fn upload(mut request: Request<Arc<State>>) -> TideResult {
+    let content_type = request.content_type().unwrap();
+    let boundary = match content_type.param("boundary") {
+        Some(b) if content_type.essence() != mime::MULTIPART_FORM.essence() => b.as_str(),
+        _ => return Ok(Response::builder(StatusCode::BadRequest).body("Invalid multipart request").build()),
+    };
+
+    let body = request.body_bytes().await?;
+    let mut multipart = Multipart::with_body(Cursor::new(&body[..]), boundary);
+
+    let mut file = None;
+    while let Some(mut mpf) = multipart.read_entry()? {
+        let field_name = mpf.headers.name.as_ref();
+        let filename = mpf.headers.filename;
+        match (field_name, filename) {
+            ("upload_file", Some(filename)) => {
+                let mut bytes = vec![];
+                mpf.data.read_to_end(&mut bytes)?;
+                file = Some((bytes, filename));
+            }
+            _ => (),
+        }
+    }
+    todo!();
+}
--- src/endpoint/mod.rs
+++ src/endpoint/mod.rs
@@ -1,6 +1,7 @@
 //! Contains root-level endpoints.
 
 pub(crate) mod auth;
+pub(crate) mod media;
 
 use crate::{
     application::State,
@@ -36,41 +37,3 @@ pub async fn add_flash(mut request: Request<Arc<State>>) -> TideResult {
 
     Ok(Redirect::new("/").into())
 }
-
-/*
-async fn index_test(mut request: Request<()>) -> TideResult {
-    let content_type = request.content_type().unwrap();
-    if content_type.essence() != mime::MULTIPART_FORM.essence() {
-        return Ok(Response::builder(StatusCode::BadRequest)
-            .body("You must upload at least one file")
-            .build());
-    }
-    let boundary = match content_type.param("boundary") {
-        Some(b) => b.as_str(),
-        None => return Ok(Response::builder(StatusCode::BadRequest).body("Invalid multipart request").build()),
-    };
-
-    let body = request.body_bytes().await?;
-    let wrapped_body = Cursor::new(&body[..]);
-    let mut multipart = Multipart::with_body(wrapped_body, boundary);
-
-    loop {
-        let part = match multipart.read_entry()? {
-            Some(part) => part,
-            None => break,
-        };
-        let MultipartField { headers, mut data } = part;
-
-        match headers.filename {
-            Some(f) => {
-                let mut bytes = vec![];
-                data.read_to_end(&mut bytes)?;
-                info!("File {} given, size was {} bytes", f, bytes.len())
-            }
-            None => info!("Non-file given"),
-        }
-    }
-
-    todo!();
-}
-*/
